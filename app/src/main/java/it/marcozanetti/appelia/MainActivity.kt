5d522f50b22ae27571ee2364edc1b19262f2e892
Marco Zanetti <marco.zanetti@synesthesia.it>
2021-07-30 23:52:54 +0200

    Added font, icon, flow between colors

--- app/src/main/java/it/marcozanetti/appelia/MainActivity.kt
+++ app/src/main/java/it/marcozanetti/appelia/MainActivity.kt
@@ -1,16 +1,22 @@
 package it.marcozanetti.appelia
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.Window
+import android.widget.AdapterView
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import java.util.*
-import android.content.DialogInterface
-import android.util.Log
+import androidx.core.graphics.ColorUtils
+import androidx.window.WindowManager
 import eltos.simpledialogfragment.SimpleDialog
 import eltos.simpledialogfragment.color.SimpleColorDialog
+import eltos.simpledialogfragment.color.SimpleColorDialog.COLORFUL_COLOR_PALLET
+import eltos.simpledialogfragment.color.SimpleColorDialog.MATERIAL_COLOR_PALLET_LIGHT
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(), SimpleDialog.OnDialogResultListener {
@@ -31,11 +37,11 @@ class MainActivity : AppCompatActivity(), SimpleDialog.OnDialogResultListener {
         eliaText.setTextColor(getRandomColor())
 
         background.setOnClickListener {
-            background.setBackgroundColor(getRandomColor())
+            background.setBackgroundColor(ColorUtils.blendARGB(background.solidColor, getRandomColor(), 1.0f));
         }
 
         eliaText.setOnClickListener {
-            eliaText.setTextColor(getRandomColor())
+            eliaText.setTextColor(ColorUtils.blendARGB(eliaText.solidColor, getRandomColor(), 1.0f));
         }
 
         changeBackgroundColorButton.setOnClickListener {
@@ -55,20 +61,22 @@ class MainActivity : AppCompatActivity(), SimpleDialog.OnDialogResultListener {
 
     fun getColor(tag: String) {
         SimpleColorDialog.build()
+            .choiceMode(SimpleColorDialog.SINGLE_CHOICE_DIRECT)
+            .colorPreset(COLORFUL_COLOR_PALLET)
             .title(R.string.pick_a_color)
             .colorPreset(Color.RED)
             .allowCustom(true)
-            .show(this, tag);
+            .show(this, tag)
     }
 
     override fun onResult(dialogTag: String, which: Int, extras: Bundle): Boolean {
         when(dialogTag) {
             BACKGROUND_COLOR -> {
-                background.setBackgroundColor(extras.getInt(SimpleColorDialog.COLOR))
+                background.setBackgroundColor(ColorUtils.blendARGB(background.solidColor, extras.getInt(SimpleColorDialog.COLOR), 1.0f));
                 return true
             }
             TEXT_COLOR -> {
-                eliaText.setTextColor(extras.getInt(SimpleColorDialog.COLOR))
+                eliaText.setTextColor(ColorUtils.blendARGB(eliaText.solidColor, extras.getInt(SimpleColorDialog.COLOR), 1.0f));
                 return true
             }
             else ->
